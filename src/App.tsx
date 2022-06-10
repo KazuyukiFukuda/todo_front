39f05f22763bb7d7f2714769a80bcb3d5ff05900
KazuyukiFukuda <kazu.fukuda.1003@gmail.com>
2022-06-10 13:41:17 +0900

    Edit feed page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 //components
 import Auth from "./components/Auth";
 import Feed from "./components/Feed";
 //function
-import { signOut, signUp } from "./lib/api/auth";
-import { SignUpData } from "./interfaces";
+
 import { useSelector, useDispatch } from "react-redux";
-import { selectUser, login, logout } from "./features/userSlice";
+import { selectUser, login } from "./features/userSlice";
 import { me } from "./lib/api/users";
 
 const App: React.FC = () => {
@@ -16,7 +14,6 @@ const App: React.FC = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     if (user.id === "") {
-      console.log("user id is nothing");
       me().then((res) => {
         dispatch(
           login({
@@ -27,7 +24,6 @@ const App: React.FC = () => {
         );
       });
     }
-    console.log(user);
   }, [user.id]);
 
   return <>{user.id ? <Feed></Feed> : <Auth></Auth>}</>;
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  createContext,
+  SetStateAction,
+} from "react";
 import { TaskData } from "../interfaces";
 import { getTasks } from "../lib/api/tasks";
 import { selectUser, login, logout } from "../features/userSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { format, getDate } from "date-fns";
+//import { PageStatus } from "../interfaces";
 
 import Task from "./Task";
 import Header from "./Header";
+import CreateTask from "./CreateTask";
+import EditTask from "./EditTask";
+import { Button } from "@material-ui/core";
+import { render } from "@testing-library/react";
+import EditProfile from "./EditProfile";
+
+export const enum PageStatus {
+  FEED = "feed",
+  EDITPROFILE = "editing profile",
+  CREATETASK = "creating task",
+  EDITINGTASK = "editing task",
+}
+
+export const pageStatusContext = createContext<PageStatus>(PageStatus.FEED);
+export const setPageStateContext = createContext<
+  React.Dispatch<SetStateAction<PageStatus>>
+>(() => undefined);
+
+export const editingTaskIdContext = createContext<number>(1);
+export const setEditingTaskIdContext = createContext<
+  React.Dispatch<SetStateAction<number>>
+>(() => undefined);
 
 const Feed: React.FC = () => {
   const [tasks, setTasks] = useState<TaskData[]>([
@@ -21,11 +50,14 @@ const Feed: React.FC = () => {
       finished_subtask_amount: "",
     },
   ]);
-  const user = useSelector(selectUser);
+
+  const [pageState, setPageState] = React.useState<PageStatus>(PageStatus.FEED);
+  const [editingTaskId, setEditingTaskId] = React.useState<number>(0);
 
   useEffect(() => {
+    console.log("get tasks");
     const unSub = () => {
-      getTasks(user.id)
+      getTasks()
         .then(function (response) {
           setTasks(response.data.message);
         })
@@ -36,30 +68,60 @@ const Feed: React.FC = () => {
     return () => {
       unSub();
     };
-  }, []);
-  console.log(tasks);
+  }, [pageState]);
+
+  const createTaskButtonHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setPageState(PageStatus.CREATETASK);
+    console.log(pageState);
+  };
 
   return (
-    <div>
-      <Header></Header>
-      {tasks[0]?.task_id && (
-        <>
-          {tasks.map((task) => (
-            <Task
-              task_id={task.task_id}
-              name={task.name}
-              deadline={task.deadline}
-              create_user={task.create_user}
-              assignee_user={task.assignee_user}
-              public={task.public}
-              completed={task.completed}
-              total_subtask_amount={task.finished_subtask_amount}
-              finished_subtask_amount={task.finished_subtask_amount}
-            ></Task>
-          ))}
-        </>
-      )}
-    </div>
+    <pageStatusContext.Provider value={pageState}>
+      <setPageStateContext.Provider value={setPageState}>
+        <Header />
+
+        {pageState === PageStatus.FEED && (
+          <div>
+            <div style={{ margin: "10px 40px", textAlign: "right" }}>
+              <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                onClick={createTaskButtonHandler}
+              >
+                新規タスクを作成
+              </Button>
+            </div>
+            {tasks[0]?.task_id && (
+              <setEditingTaskIdContext.Provider value={setEditingTaskId}>
+                {tasks.map((task) => (
+                  <Task
+                    task_id={task.task_id}
+                    name={task.name}
+                    deadline={task.deadline}
+                    create_user={task.create_user}
+                    assignee_user={task.assignee_user}
+                    public={task.public}
+                    completed={task.completed}
+                    total_subtask_amount={task.finished_subtask_amount}
+                    finished_subtask_amount={task.finished_subtask_amount}
+                  />
+                ))}
+              </setEditingTaskIdContext.Provider>
+            )}
+          </div>
+        )}
+        {pageState === PageStatus.EDITINGTASK && (
+          <editingTaskIdContext.Provider value={editingTaskId}>
+            <EditTask />
+          </editingTaskIdContext.Provider>
+        )}
+
+        {pageState === PageStatus.CREATETASK && <CreateTask />}
+        {pageState === PageStatus.EDITPROFILE && <EditProfile />}
+      </setPageStateContext.Provider>
+    </pageStatusContext.Provider>
   );
 };
 export default Feed;
